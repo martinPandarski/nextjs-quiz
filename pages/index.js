cef59ddb246c51147b80460ac163879197011f2c
martinPandarski <marto65481@gmail.com>
2021-08-23 17:57:52 +0300

    added general logic to the quiz with dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,93 @@
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import styles from "../styles/Quiz.module.css";
 
+const questions = [
+  {
+    questionText: "Which of these is the capital of France?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false, index: "A" },
+      { answerText: "London", isCorrect: false, index: "B" },
+      { answerText: "Paris", isCorrect: true, index: "C" },
+      { answerText: "Dublin", isCorrect: false, index: "D" },
+    ],
+  },
+  {
+    questionText: "Who is CEO of Tesla?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false, index: "A" },
+      { answerText: "London", isCorrect: false, index: "B" },
+      { answerText: "Paris", isCorrect: true, index: "C" },
+      { answerText: "Dublin", isCorrect: false, index: "D" },
+    ],
+  },
+  {
+    questionText: "The iPhone was created by which company?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false, index: "A" },
+      { answerText: "London", isCorrect: false, index: "B" },
+      { answerText: "Paris", isCorrect: true, index: "C" },
+      { answerText: "Dublin", isCorrect: false, index: "D" },
+    ],
+  },
+  {
+    questionText: "How many Harry Potter books are there?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false, index: "A" },
+      { answerText: "London", isCorrect: false, index: "B" },
+      { answerText: "Paris", isCorrect: true, index: "C" },
+      { answerText: "Dublin", isCorrect: false, index: "D" },
+    ],
+  },
+];
+
 function Quiz() {
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currQuestion, setCurrQuestion] = useState(0);
+
+  const answerButtonHandler = (isCorrect) => {
+    if(isCorrect){
+        setScore(score + 1)
+    }
+    const toggleNextQuestion = currQuestion + 1;
+    if (toggleNextQuestion < questions.length) {
+      setCurrQuestion(toggleNextQuestion);
+    } else {
+      setShowScore(true)
+    }
+  };
+
   return (
     <Fragment>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/sly1ocm.css" />
       </Head>
       <div className={styles["quiz-wrapper"]}>
-        <p className={styles["question-title"]}>WHICH IS THE MISSING LINE?</p>
-        <p>NE ZNAM</p>
-        <div className={styles.choice}>
-          <div className={styles["choice-label"]}>
-            <span>A</span>
-          </div>
-          <div className={styles['choice-div']}>asdasdasdasd</div>
-        </div>
-        <div className={styles.choice}>
-          <div className={styles["choice-label"]}>
-            <span>B</span>
-          </div>
-         <div className={styles['choice-div']}>asdasdasdasd</div>
-        </div>
+        {showScore ? (
+          <div className={styles.score}>You scored {score} out of {questions.length}</div>
+        ) : (
+          <Fragment>
+            {" "}
+            <p className={styles["question-title"]}>
+              {questions[currQuestion].questionText}
+            </p>
+            {questions[currQuestion].answerOptions.map((answerOption, i) => (
+              <div
+                className={styles.choice}
+                key={i}
+                onClick={() => answerButtonHandler(answerOption.isCorrect)}
+              >
+                <div className={styles["choice-label"]}>
+                  <span>{answerOption.index}</span>
+                </div>
+                <div className={styles["choice-div"]}>
+                  {answerOption.answerText}
+                </div>
+              </div>
+            ))}
+          </Fragment>
+        )}
       </div>
     </Fragment>
   );
