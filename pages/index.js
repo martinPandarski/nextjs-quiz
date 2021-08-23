c745fb78af0515164a8223d5bf300f1c717f768e
martinPandarski <marto65481@gmail.com>
2021-08-23 16:37:08 +0300

    added styling to quiz

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import Head from "next/head";
+import { Fragment } from "react";
+import styles from "../styles/Quiz.module.css";
 
-
-function Quiz(){
-    
+function Quiz() {
+  return (
+    <Fragment>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/sly1ocm.css" />
+      </Head>
+      <div className={styles["quiz-wrapper"]}>
+        <p className={styles["question-title"]}>WHICH IS THE MISSING LINE?</p>
+        <p>NE ZNAM</p>
+        <div className={styles.choice}>
+          <div className={styles["choice-label"]}>
+            <span>A</span>
+          </div>
+          <div className={styles['choice-div']}>asdasdasdasd</div>
+        </div>
+        <div className={styles.choice}>
+          <div className={styles["choice-label"]}>
+            <span>B</span>
+          </div>
+         <div className={styles['choice-div']}>asdasdasdasd</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
+
+export default Quiz;
